622236cb9a371776e92c638db82ca6a50bfc1269
Edward Kotarski <edktrsk@gmail.com>
2023-10-13 09:05:06 +0100

    no ... don't

--- blockwork/containers/container.py
+++ blockwork/containers/container.py
@@ -231,7 +231,7 @@ class Container:
         if not command:
             raise ContainerError("No command provided to execute")
         # Disable interactive if not a terminal
-        interactive &= any(f and f.isatty() for f in (sys.stdout, sys.stderr))
+        interactive &= sys.stdout and sys.stdout.isatty()
         # Pickup default working directory if not set
         workdir = workdir or self.workdir
         # Make sure the local bind paths exist
