36af2a3ed36db4f72fee98003ac3a12661384bd9
Peter Birch <peter.birch@vypercore.com>
2024-09-26 14:05:46 +0100

    Fixing copyright header

--- example/infra/tools/objstore.py
+++ example/infra/tools/objstore.py
@@ -1,4 +1,4 @@
-# Copyright 2024, Peter Birch, mailto:peter@intuity.io
+# Copyright 2023, Blockwork, github.com/intuity/blockwork
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
